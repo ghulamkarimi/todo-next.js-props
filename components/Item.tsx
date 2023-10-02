10718d2629092a3e883c30819bd8496875e654aa
ghulamkarimi <karimiamirhossein12@gmail.com>
2023-10-02 02:35:52 +0200

    fertig

---
+++ components/Item.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import { IItem } from "../interface"
+import { FaTrashAlt } from 'react-icons/fa';
+import { AiFillEdit, AiOutlineCheck } from 'react-icons/ai';
+
+interface IItemProps {
+    items: IItem[],
+    setItems: React.Dispatch<React.SetStateAction<IItem[]>>,
+    filter: string,
+    setFilter: React.Dispatch<React.SetStateAction<string>>,
+}
+const Item = ({ items, setItems, filter, setFilter }: IItemProps) => {
+
+    const editVorItem = (id: string) => {
+        const index = items.findIndex((item) => item.id === id);
+        const newItems = [...items]
+        setFilter(newItems[index].title)
+        newItems[index].done = !newItems[index].done;
+    };
+
+    const deleteItem = (id: string) => {
+        const newItem = items.filter((items) => items.id !== id);
+        setItems(newItem)
+    }
+
+    const editItem = (id: string) => {
+        const index = items.findIndex((item) => item.id === id);
+        const newItems = [...items]
+        newItems[index].title = filter;
+        newItems[index].done = !newItems[index].done;
+        setItems(newItems)
+        setFilter("")
+    }
+    return (
+        <ul className='bg-slate-600 flex rounded-xl flex-col  justify-center items-center mt-8 px-2'>
+            {
+                items.map((item) => (
+                    <li className='flex px-4  gap-48 bg-white my-2 py-2 border-b last:border-b-0 rounded-lg w-fit '
+                        key={item.id}
+
+                    >{item.title}
+                        <span className='flex items-center gap-4'>
+                            <FaTrashAlt className="text-xl text-red-400 cursor-pointer" onClick={() => { deleteItem(item.id) }} />
+                            <AiFillEdit className={`text-2xl text-orange-500 cursor-pointer ${item.done ? "hidden" : "flex"}`} onClick={() => { editVorItem(item.id) }} />
+                            < AiOutlineCheck className={`text-2xl text-green-500 cursor-pointer ${item.done ? "flex" : "hidden"}`} onClick={() => { editItem(item.id) }} />
+                        </span>
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
+
+export default Item
---
+++ components/TopForm.tsx
@@ -0,0 +1,36 @@
+import { IItem } from '@/interface';
+import React from 'react'
+import Item from './Item';
+
+interface ITopForm {
+    filter: string;
+    setFilter: React.Dispatch<React.SetStateAction<string>>;
+    items: IItem[];
+    setItems: React.Dispatch<React.SetStateAction<IItem[]>>;
+}
+const TopForm = ({ filter, setFilter, items, setItems }: ITopForm) => {
+
+
+    const inputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFilter(e.target.value)
+    }
+    const add = () => {
+        setItems([...items, { id: new Date().toISOString(), title: filter, done: false }]);
+        
+        setFilter("");
+
+    }
+    return (
+        <div>
+        <div className='flex bg-slate-700  rounded-xl gap-4 '>
+            <input className='bg-transparent outline-0 px-2 text-white' type="text" value={filter} onChange={inputChange} />
+            <button className='border-l h-full flex py-2 px-4 gap-4 text-white' onClick={add}>Add</button>
+        </div>
+        <div>
+     <Item items={items} setItems={setItems} filter={filter} setFilter={setFilter} />
+        </div> 
+        </div>
+    )
+}
+
+export default TopForm
